dcb603b909b5546ac6f268c3b82ae457ceba798f
Gabriel Lacerda <gabriel.lacerda@confitec.com.br>
2025-03-03 10:38:40 -0300

    🚧 chore: update angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,24 @@
-import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { AuthService } from './domain/auth/services/auth.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideToastr } from 'ngx-toastr';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+// import { providePrimeNG } from 'primeng/config';
+// import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (authService: AuthService) => () => authService.load(),
-      deps: [AuthService],
-      multi: true,
-    },
+    provideAppInitializer(() => inject(AuthService).load()),
     provideAnimationsAsync(),
     provideToastr(),
-    providePrimeNG({
-      theme: {
-          preset: Aura
-      }
-  })
+  //   providePrimeNG({
+  //     theme: {
+  //         preset: Aura
+  //     }
+  // })
   ],
 };
